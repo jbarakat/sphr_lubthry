818b7636e291216fe0f9db4a14737e18518b631d
jbarakat <jbarakat@stanford.edu>
2016-04-21 22:49:25 -0700

    first commit

---
+++ C++/draft01/src/run.cpp
@@ -0,0 +1,15 @@
+/* MAIN PROGRAM */
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <iomanip>
+#include <cstdlib>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <gsl/gsl_sf_trig.h>
+#include <gsl/gsl_sf_log.h>
+#include "../include/diff.h"
+
+
