40e40552dfe84d12faaf7e948ef8e99ed3aea2f4
jbarakat <jbarakat@stanford.edu>
2016-05-17 20:50:17 -0700

    added lalg

--- c++/draft04/include/solv.h
+++ c++/draft04/include/solv.h
@@ -1,23 +1,14 @@
 /* NUMERICAL SOLVER
- *  Assemble the difference system using the trapezoidal method (Crank
- *  -Nicholson scheme).
+ *  Assemble the difference system using the trapezoidal method (Crank-Nicholson scheme).
  *
  * REFERENCES
  *  Lopez et al, J Coll Int Sci (1976) - gravitational spreading
+ *  Moriarty, Tuck, and Schwartz, Phys Fluids A (1991) - gravitational thinning w/surface tension
  *  
  * PARAMETERS
- *  n    [input]   time index that ranges from 0 to N-1
- *  m    [input]   space index that ranges from 0 to M-1
- *  N    [input]   number of time steps
- *  M    [input]   number of grid points
- *  k    [input]   time spacing
- *  h    [input]   grid spacing
- *  var  [input]   number of variables
- *  u    [input]   unknown function (size = M*var)
- *  p    [input]   parameters (arbitrary size)
- *  sprs [input]	 indicator for whether or not to exploit sparsity
- *  F		 [output]	 function to be set to zero
- *  DF	 [output]	 Jacobian matrix
+ *  M		[input]			number of grid points
+ *  p		[input]			parameters
+ *  u		[input]			solution vector
  */
 
 #ifndef SOLV_H
@@ -31,137 +22,127 @@
 #include <cblas.h>
 #include <math.h>
 #include "./diff.h"
+#include "./lalg.h"
 
 using namespace std;
 
 /* PROTOTYPES */
-void syst_F_DF(int sprs, int dim, int var, int n,        int M, double h, double k, double *p, double *u0, double *u1, double *F, double *DF);
-void syst_F   (          int dim, int var, int n, int m, int M, double h, double k, double *p, double *u0, double *u1, double *F            );
-void syst_F   (          int dim, int var, int n,        int M, double h, double k, double *p, double *u0, double *u1, double *F            );
-void syst_DF  (int sprs, int dim, int var, int n,        int M, double h, double k, double *p, double *u0, double *u1, double *F, double *DF);
 
 /* IMPLEMENTATIONS */
 
-// 
+// prediction
+void solv_pred(int M, double dx, double dt, double *p, double *u0, double *u1){
+	int    i, j;
+	int    R = M-2; // system size
+	double a0[R], b0[R], c0[R], d0[R], e0[R], f0[R];
+	double a [R], b [R], c [R], d [R], e [R], f [R];
+	double k0[R], k[R];
+	double v0[R], v[R];
+	double dv;
+	double tol = 1e-6;
 
-// calculate F and DF
-void syst_F_DF(int sprs, int dim, int var, int n, int M, double h, double k,
-	             double *p, double *u0, double *u1, double *F, double *DF){
-	syst_F (      dim, var, n, M, h, k, p, u0, u1, F);
-	syst_DF(sprs, dim, var, n, M, h, k, p, u0, u1, F, DF);
-}
+	// parameters
+	double B   = p[0];	// Bond number
+	double U   = p[1];	// far-field thickness
+	double cf1 = 0.25*dt/dx;
+	double cf2 = cf1/(B*dx*dx*dx);
 
-// calculate F
-void syst_F(int dim, int var, int n, int m, int M, double h, double k,
-            double *p, double *u0, double *u1, double *F){
-	// ASSUME HAVE 2 VARIABLES
-	if (var == 2){
-		// arrange dependent variables as such:
-		//  u = [H0, Q0, H1, Q1, ..., Hm, Qm, ..., HM-1, QM-1]
-		int i;
-		double H1[M], Q1[M];
-		double H0[M], Q0[M];
-		double lhsH, rhsH, lhsQ, rhsQ;
-
-		// initialize H, Q
-		for (i = 0; i < M; i++){
-			H0[i] = u0[2*i  ];
-			H1[i] = u1[2*i  ];
-			Q0[i] = u0[2*i+1];
-			Q1[i] = u1[2*i+1];
-		}
-		
-		// boundary conditions
-		if      (m == 0  ){	
-			lhsH = H1[m]; rhsH = 0.0;
-			lhsQ = Q1[m]; rhsQ = 0.0;
-		}
-		else if (m == M-1){
-			double d1H1;
-			double cf2 = p[1];	// far-field thickness
-
-			diff_d1(m, M, h, H1, d1H1);
-			
-			lhsH = H1[m]; rhsH = cf2;
-			lhsQ = d1H1 ; rhsQ = 0.0;
-		}
-		
-		// interior nodes
-		else              {
-			double d1Q0, d1Q1, d3H1, H12, H13;
-			double w = 0.5*k; 		// trapezoidal weight
-			double cf1 = p[0];		// reciprocal Bond number
-
-			diff_d1(m, M, h, Q0, d1Q0);
-			diff_d1(m, M, h, Q1, d1Q1);
-			diff_d3(m, M, h, H1, d3H1);
-			H12 = H1[m]*H1[m];
-			H13 = H12  *H1[m];
-			
-			// mass balance
-			lhsH = H1[m] + w*d1Q1;
-			rhsH = H0[m] - w*d1Q0;
-			
-			// momentum balance + normal stress balance
-			lhsQ = Q1[m] - (1.0/3.0)*H13*(1.0 + cf1*d3H1);
-			rhsQ = 0.0;
-		}
-
-		// assemble (nonlinear) difference equation
-		F[0] = lhsH - rhsH;
-		F[1] = lhsQ - rhsQ;
+	// initialize
+	for (i = 0; i < R; i++){
+		v0[i] = u0[i];
+		k0[i] = pow(0.5*(u0[i] + u0[i+1]),3)/3.0;
 	}
-	else {
-		cout << "Should have 2 variables H and Q." << endl;
+	k0[R-1] = pow(0.5*(u0[i] + U),3)/3.0;
+
+	a0[0] = 0.0;
+	b0[0] = 0.0;
+	c0[0] = 
+	d0[0] = 
+	e0[0] = 
+	f0[0] = 
+
+	a0[1] = 0.0;
+	b0[1] = 
+	c0[1] = 
+	d0[1] = 
+	e0[1] = 
+	f0[1] = 
+
+	for (i = 2; i < R-2; i++){
+		a0[i] =       cf2*     k0[i-1]             ;
+		b0[i] =     - cf2*(3.0*k0[i-1] +     k0[i]);
+		c0[i] = 1.0 - cf2*(3.0*k0[i-1] + 3.0*k0[i]);
+		d0[i] =     - cf2*(    k0[i-1] + 3.0*k0[i]);
+		e0[i] =       cf2*                   k0[i] ;
+		f0[i] = 
 	}
-}
 
-// calculate F
-void syst_F(int dim, int var, int n, int M, double h, double k,
-            double *p, double *u0, double *u1, double *F){
-
-	int m, v;
-	double Fm[var];
+	a0[R-2] = 
+	b0[R-2] = 
+	c0[R-2] = 
+	d0[R-2] = 
+	e0[R-2] = 0.0;
+	f0[R-2] = 
 	
-	for (m = 0; m < M; m++){
-		syst_F(dim, var, n, m, M, h, k, p, u0, u1, Fm);
-		for (v = 0; v < var; v++){
-			F[var*m + v] = Fm[v];
-		}
-	}
-}
-
-// Jacobian matrix DF = dF/du1 (all xmi, xmj)
-//  NOTE: sprs = sparsity pattern
-void syst_DF(int sprs, int dim, int var, int n, int M, double h, double k,
-             double *p, double *u0, double *u1, double *F, double *DF){
-	int m, v, i, j;
-	int Mv = var*M;
-	double u1p[Mv];
-	double Fp[var], DFp[var];
-	double du1 = 0.0001;
-
-	/*------ CALCULATE PARTIAL DERIVATIVES ------*/
-	/*------  WITHOUT EXPLOITING SPARSITY  ------*/
-	if (sprs == 0){
-		for (m = 0; m < M; m++){			// element of F
-			for (i = 0; i < Mv; i++){		// element of u
-				// perturb u1
-				for (j = 0; j < Mv; j++)
-					u1p[j] = u1[j];
-				u1p[i] += du1;
-
-				syst_F(dim, var, n, m, M, h, k, p, u0, u1p, Fp);
-				for (v = 0; v < var; v++){
-					DFp[v]                  = (Fp[v] - F[var*m + v])/du1;
-					DF [(var*m + v)*Mv + i] = DFp[v];
-				}
-			}
-		}
-	}
+	a0[R-1] = 
+	b0[R-1] = 
+	c0[R-1] = 
+	d0[R-1] = 0.0;
+	e0[R-1] = 0.0;
+	f0[R-1] = 
 	
-	/*------ CALCULATE PARTIAL DERIVATIVES ------*/
-	/*------      EXPLOITING SPARSITY      ------*/
+	// iterate
+	for (i = 0; i < R-1; i++)
+		k[i] = pow(0.25*(u0[i  ] + v0[i  ] + u0[i+1] + v0[i+1]),3)/3.0;
+	k[R-1] = pow(0.25*(u0[R-1] + v0[R-1] + 2.0*U),3)/3.0;
+
+	// compute coefficients for pentadiagonal system
+	a[0]   = 0.0               ;
+	b[0]   = 0.0               ;
+	c[0]   = 1.0 + 4.0*cf2*k[0];
+	d[0]   =     - 3.0*cf2*k[0];
+	e[0]   =           cf2*k[0]; 
+	f[0]   = 
+
+	a[1]   = 0.0                            ;
+	b[1]   =     - cf2*(3.0*k[0] +     k[1]);
+	c[1]   = 1.0 + cf2*(3.0*k[0] + 3.0*k[1]);
+	d[1]   =     - cf2*(    k[0] + 3.0*k[1]);
+	e[1]   =       cf2*                k[1] ;
+	f[1]   = 
+
+	for (i = 2; i < R-2; i++){
+		a[i] =       cf2*     k[i-1]            ;
+		b[i] =     - cf2*(3.0*k[i-1] +     k[i]);
+		c[i] = 1.0 + cf2*(3.0*k[i-1] + 3.0*k[i]);
+		d[i] =     - cf2*(    k[i-1] + 3.0*k[i]);
+		e[i] =       cf2*                  k[i] ;
+		f[i] = 
+	}
+
+	a[R-2] =       cf2*     k[R-3]              ;
+	b[R-2] =     - cf2*(3.0*k[R-3] +     k[R-2]);
+	c[R-2] = 1.0 + cf2*(3.0*k[R-3] + 3.0*k[R-2]);
+	d[R-2] =     - cf2*(    k[R-3] + 3.0*k[R-2]);
+	e[R-2] = 0.0                                ;
+	f[R-2] = 
+	
+	a[R-1] =       cf2*     k[R-2]              ;
+	b[R-1] =     - cf2*(3.0*k[R-2] +     k[R-1]);
+	c[R-1] = 1.0 + cf2*(3.0*k[R-2] + 3.0*k[R-1]);
+	d[R-1] = 0.0                                ;
+	e[R-1] = 0.0                                ;
+	f[R-1] = 
+
+	// solve the pentadiagonal system for v
+
+	// compute norm of difference ||v - v0||
+	
 }
+
+// correction
+void solv_corr(){
+}
+
 
 #endif
